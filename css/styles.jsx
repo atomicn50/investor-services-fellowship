c4d0dfc19902424a84c29194666c038fd65f2945
Tinh <tinhkle@Doxie.local>
2018-12-10 23:03:20 -0800

    adds header component, adds styling using react styled-components

---
+++ css/styles.jsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+const HeaderStyle = styled.section`
+  display: inline-flex;
+`;
+
+const ColumnOne = styled.section`
+  font-size: 20px;
+  margin-right: 100px;
+`;
+
+const ColumnTwo = styled.section`
+  font-size: 20px;
+  margin-right: 300px;
+`;
+
+
+const ColumnThree = styled.section`
+  font-size: 20px;
+  margin-right: 100px;
+`;
+
+
+const ColumnFour = styled.section`
+  font-size: 20px;
+  margin-right: 100px;
+`;
+
+
+const ColumnFive = styled.section`
+  font-size: 20px;
+  margin-right: 100px;
+`;
+
+export {
+  HeaderStyle,
+  ColumnOne,
+  ColumnTwo,
+  ColumnThree,
+  ColumnFour,
+  ColumnFive,
+}
